a978adb3570cfe35fc422aac67a771dc2b9f5393
MugenZeta <hgarner213@gmail.com>
2026-07-23 17:10:18 -0500

    Start Solution for mouse interaction.

--- Scripts/s_player.gd
+++ Scripts/s_player.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 @export var turns = 5
 @export var jump_height = 20
 @export var jump_force = -sqrt(2 * gravity * jump_height)
+var inArea = false
 var isAlive = true
 @onready var Clickable_Area = $Area2D/MouseCollision
 
@@ -15,7 +16,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	velocity.y += ((delta / acceleration) * speed) * gravity
-	print(velocity.y)
+	#print(velocity.y)
 
 	var collision_info = move_and_collide(velocity * delta)
 	
@@ -24,14 +25,17 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector2(0, jump_height)
 		velocity = velocity.bounce(collision_info.get_normal())
 	
-	var mouse_Pos = get_local_mouse_position()
-	Clickable_Area.mouse
-	
-func _mouse_enter() -> void:
-	if MOUSE_BUTTON_LEFT:
-		velocity = Vector2(0, jump_height)
-		
-		
-	
-	
-	
+	while inArea == true:
+		velocity = Vector2(0,0)
+
+
+
+func _on_area_2d_mouse_entered() -> void:
+	inArea = true
+	velocity = Vector2(0,0)
+	pass # Replace with function body.
+
+
+func _on_area_2d_mouse_exited() -> void:
+	inArea = false
+	pass # Replace with function body.
