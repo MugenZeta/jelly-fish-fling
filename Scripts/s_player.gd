127467092d66d6f36160754495db20112685a6c5
Harold <hgarner213@gmail.com>
2026-07-23 06:24:24 -0500

    Working on Mouse Code

--- Scripts/s_player.gd
+++ Scripts/s_player.gd
@@ -4,8 +4,11 @@ extends CharacterBody2D
 @export var speed = 50
 @export var acceleration = 120
 @export var max_speed = 250
-@export var turns = 100
+@export var turns = 5
+@export var jump_height = 20
+@export var jump_force = -sqrt(2 * gravity * jump_height)
 var isAlive = true
+@onready var Clickable_Area = $Area2D/MouseCollision
 
 func _ready() -> void:
 	pass
@@ -15,7 +18,20 @@ func _physics_process(delta: float) -> void:
 	print(velocity.y)
 
 	var collision_info = move_and_collide(velocity * delta)
-	if collision_info: # We only want to bounce if the ball actually collided.
+	
+	#Ball Bounce
+	if collision_info:
+		velocity = Vector2(0, jump_height)
 		velocity = velocity.bounce(collision_info.get_normal())
+	
+	var mouse_Pos = get_local_mouse_position()
+	Clickable_Area.mouse
+	
+func _mouse_enter() -> void:
+	if MOUSE_BUTTON_LEFT:
+		velocity = Vector2(0, jump_height)
+		
+		
+	
 	
 	
