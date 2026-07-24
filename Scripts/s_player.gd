54470c1e4a6d843f4704f75749cf46ac567570d7
Harold <hgarner213@gmail.com>
2026-07-23 21:18:05 -0500

    WIP physics arc

--- Scripts/s_player.gd
+++ Scripts/s_player.gd
@@ -5,12 +5,12 @@ extends CharacterBody2D
 @export var acceleration = 120
 @export var max_speed = 250
 @export var turns = 5
-@export var jump_height = 20
+@export var jump_height = 100
 @export var jump_force = -sqrt(2 * gravity * jump_height)
 var inArea = false
 var isAlive = true
 @onready var Clickable_Area = $Area2D/MouseCollision
-
+@onready var sprites = $Sprite2D
 func _ready() -> void:
 	pass
 
@@ -19,23 +19,10 @@ func _physics_process(delta: float) -> void:
 	#print(velocity.y)
 
 	var collision_info = move_and_collide(velocity * delta)
+	look_at(get_global_mouse_position())
 	
 	#Ball Bounce
 	if collision_info:
-		velocity = Vector2(0, jump_height)
+		velocity = Vector2(randf_range(500, 10), jump_height)
 		velocity = velocity.bounce(collision_info.get_normal())
 	
-	while inArea == true:
-		velocity = Vector2(0,0)
-
-
-
-func _on_area_2d_mouse_entered() -> void:
-	inArea = true
-	velocity = Vector2(0,0)
-	pass # Replace with function body.
-
-
-func _on_area_2d_mouse_exited() -> void:
-	inArea = false
-	pass # Replace with function body.
