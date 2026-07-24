83aa06c560294895915e1b7bd743dea0835c3c77
Tito <matalkarp@gmail.com>
2026-07-24 09:00:02 -0400

    i don't know all i added but we have an options button

--- Maps/m_mainmenu.gd
+++ Maps/m_mainmenu.gd
@@ -15,8 +15,8 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Maps/m_TestMap.tscn")
 
 
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file()
+#func _on_options_pressed() -> void:
+	#get_tree().change_scene_to_file()
 
 
 func _on_quit_pressed() -> void:
