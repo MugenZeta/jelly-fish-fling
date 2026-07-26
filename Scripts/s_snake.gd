16f86898b4250bae9171b447a4002bbce9e877eb
Tito <matalkarp@gmail.com>
2026-07-26 01:16:11 -0400

    added level 2 & 3

--- Scripts/s_snake.gd
+++ Scripts/s_snake.gd
@@ -1,28 +1,6 @@
 extends Area2D
 
-@export var launch_force := 900.0
 
-var hostile := false
-
-func _ready():
-	body_entered.connect(_on_body_entered)
-
-func _on_body_entered(body):
-	if !body.is_in_group("player"):
-		return
-
-	if !hostile:
-		# Spring behavior
-		body.velocity.y = -launch_force
-
-		# Turn hostile after being used
-		hostile = true
-		print("The spring is now hostile!")
-	else:
-		# Hostile behavior
-		attack_player(body)
-
-func attack_player(player):
-	print("Ouch!")
-	# Example:
-	# player.take_damage(1)
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
