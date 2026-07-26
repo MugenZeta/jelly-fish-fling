c6323c502353508c81a0d84d7df8203c73bfdfef
Harold <hgarner213@gmail.com>
2026-07-26 01:10:39 -0500

    Added Music and Sound

--- Scripts/s_main_menu.gd
+++ Scripts/s_main_menu.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var sfx_hover = $Sound/SFX_Hover
+@onready var sfx_confirm = $Sound/SFX_Confirm
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,14 +12,26 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+### START BUTTON
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Maps/m_level 1.tscn")
+	sfx_confirm.play()
+	get_tree().change_scene_to_file("res://Maps/m_Level1.tscn")
 
+func _on_start_mouse_entered() -> void:
+	sfx_hover.play()
 
+### OPTION BUTTONS
 func _on_options_pressed() -> void:
+	sfx_confirm.play()
 	get_tree().change_scene_to_file("res://Maps/m_optionsmenu.tscn")
+	
+func _on_options_mouse_entered() -> void:
+	sfx_hover.play()
 
-
+### QUIT BUTTON
 func _on_quit_pressed() -> void:
+	sfx_confirm.play()
 	get_tree().quit()
+
+func _on_quit_mouse_entered() -> void:
+	sfx_hover.play()
